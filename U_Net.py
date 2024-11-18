2e11379a9319fd5b5aed222e6667e31929555c6c
WangjiaweiY <127864206+WangjiaweiY@users.noreply.github.com>
2024-11-18 20:09:46 +0800

    修改自定义UNet

--- U_Net.py
+++ U_Net.py
@@ -1,96 +1,178 @@
 import torch
-import torch.nn as nn
+from torch import nn
+from torch import Tensor
+from torchsummary import summary
+import torch.nn.functional as F
+from typing import Union
+# Get cpu or gpu device for training
+
+
+class SeparableConv2d(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int,
+                 stride: tuple, kernel_size: tuple = (3, 3),
+                 padding: Union[str, int] = 'same') -> None:
+        super(SeparableConv2d, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size,
+                                   stride=stride, padding=padding)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1))
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+
+        return x
 
 
 class ConvBlock(nn.Module):
-    """ implement conv+ReLU two times """
-    def __init__(self, in_channels, middle_channels, out_channels):
-        super().__init__()
-        conv_relu = []
-        conv_relu.append(nn.Conv2d(in_channels=in_channels, out_channels=middle_channels,
-                                   kernel_size=3, padding=1, stride=1))
-        conv_relu.append(nn.ReLU())
-        conv_relu.append(nn.Conv2d(in_channels=middle_channels, out_channels=out_channels,
-                                   kernel_size=3, padding=1, stride=1))
-        conv_relu.append(nn.ReLU())
-        self.conv_ReLU = nn.Sequential(*conv_relu)
+    """
+    Pre-activation convblock:
+    batch norm -> activation -> weights
+    """
+    def __init__(self, in_channels: int, out_channels: int,
+                 stride: tuple = (1, 1), dropout: float = 0.1) -> None:
+        super(ConvBlock, self).__init__()
+        self.bn = nn.BatchNorm2d(in_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv = SeparableConv2d(in_channels, out_channels,
+                                    stride=stride)
+        self.drop = nn.Dropout2d(dropout, inplace=True)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.conv(x)
+        x = self.drop(x)
+
+        return x
+
+
+class UpBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int,
+                 kernel_size: tuple = (3, 3)) -> None:
+        super(UpBlock, self).__init__()
+        self.up = nn.UpsamplingNearest2d(scale_factor=2)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,
+                              padding='same')
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.up(x)
+        x = self.conv(x)
+
+        return x
+
+
+class UpConvBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int,
+                 kernel_size: tuple = (3, 3)) -> None:
+        super(UpConvBlock, self).__init__()
+        self.up = UpBlock(in_channels, out_channels, kernel_size)
+        self.convblock1 = ConvBlock(out_channels*2, out_channels)
+        self.convblock2 = ConvBlock(out_channels, out_channels)
+
+    def forward(self, x: Tensor, enc_feature: Tensor) -> Tensor:
+        x = self.up(x)
+        x = torch.cat([x, enc_feature], dim=1)
+        x = self.convblock1(x)
+        x = self.convblock2(x)
+
+        return x
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int,
+                 downsampling: bool = True) -> None:
+        super(ResBlock, self).__init__()
+        self.downsampling = downsampling
+        if self.downsampling:
+            self.down_conv = SeparableConv2d(in_channels, out_channels,
+                                             stride=(2, 2), padding='valid')
+            self.convblock1 = ConvBlock(out_channels, out_channels)
+        else:
+            self.convblock1 = ConvBlock(in_channels, out_channels)
+        self.convblock2 = ConvBlock(out_channels, out_channels)
+
+    def forward(self, x: Tensor) -> Tensor:
+        if self.downsampling:
+            # padding to maintain the power of 2 dimensionality
+            x = F.pad(x, (1, 1, 1, 1), 'constant', 0)
+            x = self.down_conv(x)
+        identity = x
+        x = self.convblock1(x)
+        x = self.convblock2(x)
+        x += identity
+
+        return x
+
+
+class Encoder(nn.Module):
+    """
+    Resnet inspired encoder
+    """
+    def __init__(self, in_channels: int, init_filter: int,
+                 depth: int) -> None:
+        super(Encoder, self).__init__()
+        self.init_filter = init_filter
+        self.in_channels = in_channels
+        self.depth = depth
+        self.conv = SeparableConv2d(in_channels, init_filter, stride=(1,1))
+        self.resblock0 = ResBlock(init_filter, init_filter, downsampling=False)
+        self.resblock = nn.ModuleList([ResBlock(self.init_filter*(2**i), self.init_filter*(2**(i+1)))
+                                       for i in range(depth)])
+        self.bn = nn.BatchNorm2d(init_filter*(2**(depth)))
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x: Tensor) -> list:
+        block_outputs = []
+        x = self.conv(x)
+        x = self.resblock0(x)
+        block_outputs.append(x)
+        for i, block in enumerate(self.resblock):
+            x = block(x)
+            if i is self.depth-1:
+                x = self.bn(x)
+                x = self.relu(x)
+            block_outputs.append(x)
+
+        return block_outputs
+
+
+class Decoder(nn.Module):
+    def __init__(self, init_filter: int,
+                 depth: int, n_class: int ) -> None:
+        super(Decoder, self).__init__()
+        self.init_filter = init_filter
+        self.depth = depth
+        self.conv = nn.Conv2d(init_filter, n_class, kernel_size=(3, 3),
+                              stride=(1, 1), padding='same')
+        self.upblock = nn.ModuleList([UpConvBlock(self.init_filter*(2**i), self.init_filter*(2**(i-1)))
+                                     for i in range(depth, 0, -1)])
+        self.bn1 = nn.BatchNorm2d(init_filter)
+        self.relu = nn.ReLU(inplace=True)
+        self.bn2 = nn.BatchNorm2d(n_class)
+
+    def forward(self, enc_feature: list) -> Tensor:
+        for i in range(len(self.upblock)):
+            if i == 0:
+                x = self.upblock[i](enc_feature[self.depth], enc_feature[self.depth-1])
+            else:
+                x = self.upblock[i](x, enc_feature[self.depth-(i+1)])
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv(x)
+        x = self.bn2(x)
+
+        return x
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channels: int,
+                 init_filter: int, depth: int, n_class: int):
+        super(UNet, self).__init__()
+        self.enc = Encoder(in_channels, init_filter, depth)
+        self.dec = Decoder(init_filter, depth, n_class)
+
     def forward(self, x):
-        out = self.conv_ReLU(x)
-        return out
-    
-class U_Net(nn.Module):
-    def __init__(self):
-        super().__init__()
+        enc_feature = self.enc(x)
+        output = self.dec(enc_feature)
 
-        # 首先定义左半部分网络
-        # left_conv_1 表示连续的两个（卷积+激活）
-        # 随后进行最大池化
-        self.left_conv_1 = ConvBlock(in_channels=3, middle_channels=64, out_channels=64)
-        self.pool_1 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.left_conv_2 = ConvBlock(in_channels=64, middle_channels=128, out_channels=128)
-        self.pool_2 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.left_conv_3 = ConvBlock(in_channels=128, middle_channels=256, out_channels=256)
-        self.pool_3 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.left_conv_4 = ConvBlock(in_channels=256, middle_channels=512, out_channels=512)
-        self.pool_4 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.left_conv_5 = ConvBlock(in_channels=512, middle_channels=1024, out_channels=1024)
-
-        # 定义右半部分网络
-        self.deconv_1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.right_conv_1 = ConvBlock(in_channels=1024, middle_channels=512, out_channels=512)
-
-        self.deconv_2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=3, padding=1, stride=2, output_padding=1)
-        self.right_conv_2 = ConvBlock(in_channels=512, middle_channels=256, out_channels=256)
-
-        self.deconv_3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, padding=1, stride=2 ,output_padding=1)
-        self.right_conv_3 = ConvBlock(in_channels=256, middle_channels=128, out_channels=128)
-
-        self.deconv_4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2, output_padding=1, padding=1)
-        self.right_conv_4 = ConvBlock(in_channels=128, middle_channels=64, out_channels=64)
-        # 最后是1x1的卷积，用于将通道数化为3
-        self.right_conv_5 = nn.Conv2d(in_channels=64, out_channels=21, kernel_size=1, stride=1, padding=0)
-
-    def forward(self, x):
-
-        # 1：进行编码过程
-        feature_1 = self.left_conv_1(x)
-        feature_1_pool = self.pool_1(feature_1)
-
-        feature_2 = self.left_conv_2(feature_1_pool)
-        feature_2_pool = self.pool_2(feature_2)
-
-        feature_3 = self.left_conv_3(feature_2_pool)
-        feature_3_pool = self.pool_3(feature_3)
-
-        feature_4 = self.left_conv_4(feature_3_pool)
-        feature_4_pool = self.pool_4(feature_4)
-
-        feature_5 = self.left_conv_5(feature_4_pool)
-
-        # 2：进行解码过程
-        de_feature_1 = self.deconv_1(feature_5)
-        # 特征拼接
-        temp = torch.cat((feature_4, de_feature_1), dim=1)
-        de_feature_1_conv = self.right_conv_1(temp)
-
-        de_feature_2 = self.deconv_2(de_feature_1_conv)
-        temp = torch.cat((feature_3, de_feature_2), dim=1)
-        de_feature_2_conv = self.right_conv_2(temp)
-
-        de_feature_3 = self.deconv_3(de_feature_2_conv)
-
-        temp = torch.cat((feature_2, de_feature_3), dim=1)
-        de_feature_3_conv = self.right_conv_3(temp)
-
-        de_feature_4 = self.deconv_4(de_feature_3_conv)
-        temp = torch.cat((feature_1, de_feature_4), dim=1)
-        de_feature_4_conv = self.right_conv_4(temp)
-
-        out = self.right_conv_5(de_feature_4_conv)
-
-        return out
-    
+        return output
